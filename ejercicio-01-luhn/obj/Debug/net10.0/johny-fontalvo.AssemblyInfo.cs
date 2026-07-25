18ec81d5c66abf62c583b033fa9163daf6cb160c
Johny Fontalvo <johnyfontmu@gmail.com>
2026-07-24 20:15:52 -0500

    Mover archivos del proyecto a una subcarpeta

--- ejercicio-01-luhn/obj/Debug/net10.0/johny-fontalvo.AssemblyInfo.cs
+++ ejercicio-01-luhn/obj/Debug/net10.0/johny-fontalvo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("johny-fontalvo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef4b8860ff72b471869a94782ec5d91a412feae0")]
 [assembly: System.Reflection.AssemblyProductAttribute("johny-fontalvo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("johny-fontalvo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
